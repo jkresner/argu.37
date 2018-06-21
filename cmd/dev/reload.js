df3122a3ee4ffe420ad8e6e22725fad6e1518e9f
Jonathon Kresner <jkresner@users.noreply.github.com>
2018-06-21 14:46:54 +1000

    temp cmd

---
+++ cmd/dev/reload.js
@@ -0,0 +1,55 @@
+const minilr                     = require('mini-lr')
+const fs                         = require('fs')
+const port                       = 35729
+const paths                      = [
+ 'fs/static/css',
+ 'fs/static/js',
+ 'fs/md',
+ 'fs/img',
+ 'reload.log'
+]
+
+
+function throttle(delay, fn) {
+  let now = Date.now()
+  return function() {
+    var from = Date.now()
+    var interval = from - now
+    if (interval < delay) return
+    now = from
+    fn.apply(this, arguments)
+  }
+}
+
+
+fs.writeFileSync(__dirname+'/../../reload.log','',{mode:2})
+
+
+function watchpath(path, em) {
+  fs.watch(path, throttle(2000, (ev, file) => {
+    console.log(`dev  FS  ${ev}\t${file}`)
+    minilr.changed(file)
+    // em.emit(ev, filename)
+  }))
+}
+
+
+var watch = (function watch(em) {
+  em = em || new (require('events').EventEmitter)()
+  // em.on('change', file => {
+  //   console.log('fs.changed\t', file)
+  //   minilr.changed(file)
+  // })
+  // em.on('rename', file => {
+  //   console.log('fs.rename\t', file)
+  //   minilr.changed(file)
+  // })
+
+  for (let p of paths) watchpath(p, em)
+  return watch
+})()
+
+
+minilr().listen(port, () =>
+  console.log(`dev  WEB live-reload\t      http://localhost:${port}`)
+)
