ddf8263ece4e8552a5d3fa69aed413d6d22e7412
jkresner <jkresner@gmail.com>
2018-09-29 06:42:56 -0700

    CMD/setup

--- server/routes/05_web.js
+++ server/routes/05_web.js
@@ -9,29 +9,31 @@ module.exports = (app, mw) => {
     .get('/intro', (req, res) => res.redirect(301, '/') )
 
 
-  for (let {url,idx} of toc.chapters)
-    web.get(url, mw.$.page(idx))
+  // CAL.toc()
+
+  // for (let {url,idx} of toc.chapters)
+  //   web.get(url, mw.$.page(idx))
 
 
-    web.get('/stara-library/research-history', 
-      // mw.$.nobot,
-      mw.$.session,
+  //   web.get('/stara-library/research-history',
+  //     // mw.$.nobot,
+  //     mw.$.session,
 
-      (req, res, next) => {
-        let files = []
-        require('fs').readdirSync(join(config.appDir, 'fs', 'read')).forEach(f => {
-          if (f[0]!='.')
-            files.push({
-              url:`/read/${f}`,
-              time: moment(f.split('<')[0].trim(),'YYMMDD'),
-              title:f.split('<')[1].split('>')[0]
-            })          
-        })
-        // console.log(files)
-        req.locals.r = files
-        next()
-      },
-        mw.res.page('reading', {layout:'web'})
-      )
+  //     (req, res, next) => {
+  //       let files = []
+  //       require('fs').readdirSync(join(config.appDir, 'fs', 'read')).forEach(f => {
+  //         if (f[0]!='.')
+  //           files.push({
+  //             url:`/read/${f}`,
+  //             time: moment(f.split('<')[0].trim(),'YYMMDD'),
+  //             title:f.split('<')[1].split('>')[0]
+  //           })
+  //       })
+  //       // console.log(files)
+  //       req.locals.r = files
+  //       next()
+  //     },
+  //       mw.res.page('reading', {layout:'web'})
+  //     )
 
 }
